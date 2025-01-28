c08535f713867bac8cf14c64d4af1e63efcda45e
tejeshwar-nt <tejeshwarnt@gmail.com>
2025-01-27 21:49:05 -0500

    create proj

---
+++ MASApplication/MASApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MASApplicationApp.swift
+//  MASApplication
+//
+//  Created by Tejeshwar Natarajan on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MASApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MASApplication/Screens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MASApplication
+//
+//  Created by Tejeshwar Natarajan on 1/27/25.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    AuthView()
+}
